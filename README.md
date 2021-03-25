fe86e26e3fca5553186c1b686955fd1ae75ecfd7
Camden Thomson <camden.thomson@student.allenisd.org>
2021-03-25 18:54:17 -0500

    Added additional arithmetic properties to Vector3 and basic structure for Quaternion.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Scenes3D
 
-_Scenes3D_ provides a Swift object library with support for 3D components.  _Scenes3D_ runs on top of Scenes and IGIS.
+_Scenes3D_ provides a Swift object library with support for 3D renderable entities.  _Scenes3D_ runs on top of Scenes and IGIS.
 
 ## Proposed Properties
-- Vector3: stores coordinates in 3d space for size, scale, position, etc.
-- Orientation: stores coordinates in 3d space for rotation (accessible in degrees and radians)
-- Region3: (similar to rect) stores, size, position, and orientation of a 3d rectangular prism
+- Orientation/Quaternion: stores coordinates in 3d space for rotation (accessible in degrees and radians)
+- Region3: (similar to rect) stores size, position, and orientation of a 3d rectangular prism
 
 ## Proposed Objects
-- Layer3D: a Scenes layer with support for 3D objects (preferably not able to render 2D components)
-- Model: (similar to RenderableEntity) groups 3D components together to make manipulation complex objects easier (also allows for prerendering object interactions?)
+- Layer3D: a Scenes layer with support for 3D objects (preferably not able to render standard 2D components)
+- Model: (similar to RenderableEntity) groups 3D components together to make manipulation of complex objects easier (also allows for prerendering object interactions?)
 - Part: (name change possible - similar to CanvasObject) a 3D object actually renderable on the screen.
 - Camera: stores position, rotation, and viewport, one can be assigned to Layer3D, and is in charge of determining how to render in 3d space.
 - Light: An object that defines where light sources from, along with its reach and brightness.  Initial lighting will be done on a face-to-face surface without gradients (entire faces will be the same color).
+- (internally) FaceGeometry: Stores indices for a particular face and values which dictate its resolution (how many triangles we render).
+- (internally) Triangle: Takes 3 points in 3D space and, when given data from camera, calculates 2D coordinates and renders paths.
--- Sources/Scenes3D/Vector3.swift
+++ Sources/Scenes3D/Vector3.swift
@@ -15,22 +15,78 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-public struct Vector3 {
+/// A `Vector3` represents a point in 3D space, and is typically used to define the position and size of a three-dimensional object.
+public struct Vector3 : Equatable, AdditiveArithmetic {
+    /// The coordinate along the x-axis.
     public var x : Int
+    /// The coordinate along the y-axis.
     public var y : Int
+    /// The coordinate along the z-axis.
     public var z : Int
 
+    /// The vector3 (x:0, y:0, z:0).
     static public let zero = Vector3(x:0, y:0, z:0)
 
+    /// Creates a new `Vector3` with the properties (x:0, y:0, z:0)
     public init() {
         self.x = 0
         self.y = 0
         self.z = 0
     }
 
+    /// Creates a new `Vector3` from the specified coordinates.
+    /// - Parameters:
+    ///   - x: The x-coordinate
+    ///   - y: The y-coordinate
+    ///   - z: The z-coordinate
     public init(x:Int, y:Int, z:Int) {
         self.x = x
         self.y = y
         self.z = z
+    }
+
+    /// Calculates the square of the Euclidean distance between this vector3 and another.
+    /// - Parameters:
+    ///   - target: The target vector3 to which to calculate the distance
+    /// - Returns: The square of the distance to a target vector3
+    public func distanceSquared(to target:Vector3) -> Int {
+        let xDistance = target.x - x
+        let xDistanceSquared = xDistance * xDistance
+
+        let yDistance = target.y - y
+        let yDistanceSquared = yDistance * yDistance
+
+        let zDistance = target.z - z
+        let zDistanceSquared = zDistance * zDistance
+
+        return xDistanceSquared + yDistanceSquared + zDistanceSquared
+    }
+
+    /// Calculates the Euclidean distance between this vector3 and another.
+    /// - Parameters:
+    ///   - target: The target point to which to calculate the distance
+    /// - Returns: The distance to a target vector3
+    public func distance(to target:Vector3) -> Double {
+        return sqrt(Double(distanceSquared(to:target)))
+    }
+
+    /// Equivalence operator for two `Vector3`s.
+    static public func == (left:Vector3, right:Vector3) -> Bool {
+        return left.x == right.x && left.y == right.y && left.z == right.z
+    }
+
+    /// Addition operator for two `Vector3`s.
+    static public func + (left:Vector3, right:Vector3) -> Vector3 {
+        return Vector3(x:left.x + right.x, y:left.y + right.y, z:left.z + right.z)
+    }
+
+    /// Negation operator for a `Vector3`.
+    static public prefix func - (vector3:Vector3) -> Vector3 {
+        return Vector3(x:-vector3.x, y:-vector3.y, z:-vector3.z)
+    }
+
+    /// Subtraction operator for two `Vector3`s.
+    static public func - (left:Vector3, right:Vector3) -> Vector3 {
+        return left + -right
     }
 }
